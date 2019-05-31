09deb88f1c8482daab855ecc15c5eeb13311933c
jjingbak <qurdoo228@gmail.com>
2019-05-31 12:15:10 +0900

    0531공지사항검색까지완성

---
+++ footBall/src/notice/controllor/SearchKeywordServlet.java
@@ -0,0 +1,56 @@
+package notice.controllor;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import notice.model.service.NoticeService;
+import notice.model.vo.NoticePageData;
+
+/**
+ * Servlet implementation class SearchKeywordServlet
+ */
+@WebServlet(name = "SearchKeyword", urlPatterns = { "/searchKeyword" })
+public class SearchKeywordServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchKeywordServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		int reqPage;
+		try {
+			reqPage = Integer.parseInt(request.getParameter("reqPage"));
+		}catch(NumberFormatException e) {
+			reqPage = 1;
+		}
+		String keyword = request.getParameter("keyword");
+		NoticePageData pd = new NoticeService().searchKeyword(reqPage,keyword);
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/notice/notice.jsp");
+		request.setAttribute("pd", pd);
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
