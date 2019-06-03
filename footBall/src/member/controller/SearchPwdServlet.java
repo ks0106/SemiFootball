2aa9c20ee009c75951a5714f9d522591bc83fd30
ks0106 <rudtjr008@gmail.com>
2019-06-03 20:41:37 +0900

    0603 비밀번호찾기 페이지 완료

--- footBall/src/member/controller/SearchPwdServlet.java
+++ footBall/src/member/controller/SearchPwdServlet.java
@@ -1,11 +1,17 @@
 package member.controller;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import member.model.service.MemberService;
 
 /**
  * Servlet implementation class SearchPwdServlet
@@ -26,8 +32,18 @@ public class SearchPwdServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String id = request.getParameter("id");
+		String name = request.getParameter("name");
+		
+		try {
+			String id1 = new MemberService().searchPwd(id,name);
+			response.setContentType("application/json");
+			response.setCharacterEncoding("utf-8");
+			new Gson().toJson(id1,response.getWriter());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/**
