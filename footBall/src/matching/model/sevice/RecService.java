9867fdec1f118f54b24866c9514be1d662cbf2a6
kdh062493 <kdh062493@gmail.com>
2019-06-11 16:09:06 +0900

    매치 용병모집 페이지 오류 수정

--- footBall/src/matching/model/sevice/RecService.java
+++ footBall/src/matching/model/sevice/RecService.java
@@ -121,6 +121,45 @@ public class RecService {
 		return rpd;
 	
 	}
+	public RecPageData searchList2(int reqPage, int branch, String keyword) throws SQLException {
+		Connection conn = JDBCTemplate.getConnection();
+		int numPerPage=10;
+		int totalCount= new RecDao().searchCount2(conn,branch,keyword);
+		int totalPage = (totalCount%numPerPage==0)?(totalCount/numPerPage):(totalCount/numPerPage)+1;
+		int start = (reqPage-1)*numPerPage+1;
+		int end = reqPage*numPerPage;
+		ArrayList<Recruit> list= new RecDao().searchList2(conn,start,end,branch,keyword);
+		for(int i = 0 ; i<list.size();i++) {
+			list.get(i).setAmount2(list.get(i).getAmount());
+			list.get(i).setAble2(list.get(i).getRecAble());
+			list.get(i).setDate2(list.get(i).getRecDate());
+		}
+		String pageNavi ="";
+		int pageNaviSize = 5;
+		int pageNo = ((reqPage-1)/pageNaviSize)*pageNaviSize+1;
+		
+		if(pageNo!=1) {
+			pageNavi += "<a class='btn' href='/recSearch?branch"+branch+"&keyword="+keyword+"&reqPage="+(pageNo-1)+"'><div class='pageNaviBtn'>&lt</div></a>";
+		}
+		int i = 1;
+		while(!(i++>pageNaviSize||pageNo>totalPage)) {
+			if(reqPage==pageNo) {
+				pageNavi += "<div class='pageNaviBtn selectPage'><span>"+pageNo+"</span></div>";
+			}else {
+				pageNavi +="<a class='btn' href='/recSearch?branch"+branch+"&keyword="+keyword+"&reqPage="+pageNo+"'><div class='pageNaviBtn'>"+pageNo+"</div></a>";
+			}
+			pageNo++;
+		}
+		if(pageNo <= totalPage) {
+			pageNavi += "<a class='btn' href='/recSearch?branch"+branch+"&keyword="+keyword+"&reqPage="+pageNo+"'><div class='pageNaviBtn'>&gt</div></a>";
+			
+		}
+		
+		JDBCTemplate.close(conn);
+		RecPageData rpd = new RecPageData(list, pageNavi);
+		return rpd;
+		
+	}
 	
 	public RecPageData selectList2(int reqPage) throws SQLException {
 		Connection conn = JDBCTemplate.getConnection();
